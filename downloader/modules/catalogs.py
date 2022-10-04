49d392f72ba468a14ac260984520c5d620733d78
iWas-Coder <wasymatieh01@gmail.com>
2022-10-04 22:57:47 +0200

    Developing

---
+++ downloader/modules/catalogs.py
@@ -0,0 +1,29 @@
+"""
+macOS Official Public Releases
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+(2002-2021). Hosted in: swscan.apple.com
+"""
+
+# Oldest versions
+PANTHER = "https://swscan.apple.com/scanningpoints/scanningpointX.xml"
+TIGER = "https://swscan.apple.com/content/catalogs/index.sucatalog"
+LEOPARD = "https://swscan.apple.com/content/catalogs/others/index-leopard.merged-1.sucatalog"
+SNOW_LEOPARD = "https://swscan.apple.com/content/catalogs/others/index-snowleopard.merged-1.sucatalog"
+
+# Older versions
+LION = "https://swscan.apple.com/content/catalogs/others/index-lion.merged-1.sucatalog"
+MOUNTAIN_LION = "https://swscan.apple.com/content/catalogs/others/index-mountainlion.merged-1.sucatalog"
+MAVERICKS = "https://swscan.apple.com/content/catalogs/others/index-10.9.merged-1.sucatalog"
+YOSEMITE = "https://swscan.apple.com/content/catalogs/others/index-10.10.merged-1.sucatalog"
+EL_CAPITAN = "https://swscan.apple.com/content/catalogs/others/index-10.11.merged-1.sucatalog"
+SIERRA = "https://swscan.apple.com/content/catalogs/others/index-10.12.merged-1.sucatalog"
+HIGH_SIERRA = "https://swscan.apple.com/content/catalogs/others/index-10.13.merged-1.sucatalog"
+MOJAVE = "https://swscan.apple.com/content/catalogs/others/index-10.14.merged-1.sucatalog"
+
+# Newer versions
+CATALINA = "https://swscan.apple.com/content/catalogs/others/index-10.15.merged-1.sucatalog"
+BIG_SUR = "https://swscan.apple.com/content/catalogs/others/index-10.16.merged-1.sucatalog"
+
+# Latest
+MONTEREY = "https://swscan.apple.com/content/catalogs/others/index-12.merged-1.sucatalog"
